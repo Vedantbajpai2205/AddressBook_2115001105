88ce67fca018d69494b11d41515fc9368dceb350
Vedantbajpai2205 <yashbaj987@gmail.com>
2025-03-12 23:21:45 +0530

    [Vedant]Add:adding Section1_UC1 code files into branch

---
+++ AddressBook/Controllers/AddressBookController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelLayer.Model; // Include your models
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using RepositoryLayer.Context;
+
+namespace AddressBook.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AddressBookController : ControllerBase
+    {
+
+    }
+}
---
+++ AddressBook/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using RepositoryLayer.Context;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Database Connection
+var connectionString = builder.Configuration.GetConnectionString("SqlConnection");
+builder.Services.AddDbContext<AddressContext>(options => options.UseSqlServer(connectionString));
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();

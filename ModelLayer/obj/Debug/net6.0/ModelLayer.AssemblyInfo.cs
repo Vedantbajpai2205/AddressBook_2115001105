3e1de15fc1b174ffd8d52062853a4f00a76947f8
Vedantbajpai2205 <yashbaj987@gmail.com>
2025-03-16 15:42:55 +0530

    [Vedant]Add:adding Section3_UC1 code files into branch

--- ModelLayer/obj/Debug/net6.0/ModelLayer.AssemblyInfo.cs
+++ ModelLayer/obj/Debug/net6.0/ModelLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ModelLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dab035c484d19d3f25712d798691c7527edef41a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+664eeed560ffb2e0730a61db85433b1cc7c4c516")]
 [assembly: System.Reflection.AssemblyProductAttribute("ModelLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ModelLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
